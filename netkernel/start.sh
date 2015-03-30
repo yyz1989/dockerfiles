ff034d3ccc2c0fbdd655606b6ceffc75319bbe65
Yang Yuanzhe <yyz1989@hotmail.com>
2015-03-30 22:11:45 +0200

    Moved dockerfiles into this repository

---
+++ netkernel/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+service ssh start
+java -jar 1060-NetKernel-SE-5.2.1.jar
+#/netkernel/bin/netkernel.sh
---
+++ stardog/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+service ssh start
+/stardog/bin/stardog-admin server start
