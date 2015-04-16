2d1517ebc7b650efddf0edcd0144b7663a6b296c
Yang Yuanzhe <yyz1989@hotmail.com>
2015-04-16 19:06:39 +0200

    Follow the log of MarkLogic

--- marklogic/start.sh
+++ marklogic/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+touch /var/opt/MarkLogic/Logs/8001_AccessLog.txt
 service ssh start
-/etc/init.d/MarkLogic start &
+/etc/init.d/MarkLogic start
+tail -f /var/opt/MarkLogic/Logs/8001_AccessLog.txt
