85f0e8010a93d81e65c0047ff4a73fb65724dfae
Yang Yuanzhe <yyz1989@hotmail.com>
2015-08-11 19:31:58 +0200

    Updated dockerfile for unifiedviews

--- unifiedviews/init.sh
+++ unifiedviews/init.sh
@@ -17,9 +17,6 @@ if [ ! -f /unifiedviews/core/backend/target/config.properties ]; then
     psql -h ${POSTGRES_PORT_5432_TCP_ADDR} -p ${POSTGRES_PORT_5432_TCP_PORT} -d unifiedviews -f data-core.sql
     psql -h ${POSTGRES_PORT_5432_TCP_ADDR} -p ${POSTGRES_PORT_5432_TCP_PORT} -d unifiedviews -f data-permissions.sql
     cp /unifiedviews/core/frontend/target/unifiedviews.war /var/lib/tomcat7/webapps/
-    service tomcat7 restart
-    service tomcat7 stop
-    cp /unifiedviews/config/frontend.config.properties /var/lib/tomcat7/webapps/unifiedviews/WEB-INF/config.properties    
 fi  
 
 exit 0
--- unifiedviews/start.sh
+++ unifiedviews/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 /etc/init.d/init.sh && \
-nohup java -DconfigFileLocation=/unifiedviews/core/backend/target/config.properties -jar /unifiedviews/core/backend/target/backend-2.1.1.jar && \ 
-service tomcat7 restart && \ 
+nohup java -DconfigFileLocation=/unifiedviews/core/backend/target/config.properties -jar /unifiedviews/core/backend/target/backend-2.1.1.jar & \ 
+sleep 5 & \
+service tomcat7 restart & \ 
 service ssh start && \ 
 tail -f /var/log/tomcat7/catalina.out
