5d88c224e375d276fe71f7f7f000d3f0d1002aa8
Yang Yuanzhe <yyz1989@hotmail.com>
2015-08-13 12:07:11 +0200

    add full file path for unifiedviews start script

--- unifiedviews/start.sh
+++ unifiedviews/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-init.sh
+/unifiedviews/init.sh
 nohup java -DconfigFileLocation=/unifiedviews/config/backend.config.properties -jar /unifiedviews/core/backend/target/backend-2.1.1.jar &
 sleep 5
 chmod -R 777 /unifiedviews/target
 service tomcat7 restart
 service ssh start
-dpu.sh
+/unifiedviews/dpu.sh
 tail -f /var/log/tomcat7/catalina.out
