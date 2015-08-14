4bdc62b1c5296d4b645b286365657df637213381
Yang Yuanzhe <yyz1989@hotmail.com>
2015-08-14 15:01:26 +0200

    Updated dockerfile for unifiedviews

--- unifiedviews/start.sh
+++ unifiedviews/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /unifiedviews/init.sh
-nohup java -DconfigFileLocation=/unifiedviews/config/backend.config.properties -jar /unifiedviews/core/backend/target/backend-2.1.1.jar &
+nohup java -DconfigFileLocation=/unifiedviews/config/backend.config.properties -jar /unifiedviews/core/backend/target/backend-2.1.1.jar & > /var/log/unifiedviews/nohup.out
 sleep 5
 chmod -R 777 /unifiedviews/target
 service tomcat7 restart
